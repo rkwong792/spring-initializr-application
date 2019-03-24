485cc8d7ff0087112f63761768a730a123445281
DESKTOP-5RABSME\Richard <rkwong792@gmail.com>
2019-03-24 17:22:02 -0400

    Commit by Richard Kwong 2

---
+++ springinitializrAPP/src/main/java/com/rkcode/springboot/demo/springinitializrAPP/SpringinitializrAppApplication.java
@@ -0,0 +1,13 @@
+package com.rkcode.springboot.demo.springinitializrAPP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinitializrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinitializrAppApplication.class, args);
+	}
+
+}
