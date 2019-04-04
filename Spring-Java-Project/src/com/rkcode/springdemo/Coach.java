f4f956d11a716163d7d31dcbd53827e3c34470a8
DESKTOP-5RABSME\Richard <rkwong792@gmail.com>
2019-04-04 18:15:40 -0400

    Commit by Richard Kwong

---
+++ Spring-Java-Project/src/com/rkcode/springdemo/Coach.java
@@ -0,0 +1,9 @@
+package com.rkcode.springdemo;
+
+//Re-factoring Code: Creating an Interface
+//Note: An interface doesn't have any implementation code. 
+//It's only a specification. What is available? Not how it is implemented.
+public interface Coach {
+	
+	public String getDailyWorkout();
+}
